6a4f7ce3450bf80ea62b4295cda1c4d35c8e0637
Fabrizio Martone <53048926+Blue00FF@users.noreply.github.com>
2021-09-22 23:33:13 +0100

    Add files via upload
    Created a program that takes the NASA picture/video of the day and emails it to you using the NASA API. The API works and prints the correct information to the console in the NASA_picture.py script but doesn't send it via email in the NASA_picture_email.py script.

---
+++ NASA_picture.py
@@ -0,0 +1,57 @@
+from datetime import datetime
+import requests
+import yaml
+
+
+def load_yaml(filepath):
+    with open(filepath, "r") as stream:
+        try:
+            return yaml.safe_load(stream)
+        except yaml.YAMLError as e:
+            print(e)
+
+
+sender = "NASA <NASA@totallyNASA.com>"
+receiver = "A Test User <to@example.com>"
+
+endpoint = "https://api.nasa.gov/planetary/apod"
+config = load_yaml("./config.yaml")
+api_key = config["NASA_api_key"]
+query_params = {"api_key": api_key, "date": datetime.today().strftime("%Y-%m-%d")}
+response = requests.get(endpoint, params=query_params)
+if response.status_code == 200:
+    explanation = response.json()["explanation"]
+    title = response.json()["title"]
+    media_type = response.json()["media_type"]
+    url = response.json()["url"]
+    if media_type == "image":
+        message = f"""\
+        Subject: Your Daily Astronomy Picture: {title}
+        To: {receiver}
+        From: {sender}
+
+        Hello Fabrizio,
+
+        This is the Astronomy Picture of the Day:
+
+        {explanation}
+
+        {url}
+        """
+    else:
+        message = f"""\
+        Subject: Your Daily Astronomy Video: {title}
+        To: {receiver}
+        From: {sender}
+
+        Hello Fabrizio,
+
+        This is the Astronomy Video of the Day:
+
+        {explanation}
+
+        {url}
+        """
+        print(message)
+else:
+    print(f"{response.status_code}: {response.reason}")
