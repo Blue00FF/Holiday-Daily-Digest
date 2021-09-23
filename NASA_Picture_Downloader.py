a2c126d127bf4f49aaf006ea53cba5d3f36854c0
Fabrizio Martone <53048926+Blue00FF@users.noreply.github.com>
2021-09-23 19:19:00 +0100

    Added comments to the code

--- NASA_Picture_Downloader.py
+++ NASA_Picture_Downloader.py
@@ -1,8 +1,9 @@
-from datetime import datetime
 import requests
 import yaml
+from datetime import datetime
 
 
+# define a program that safely loads the data from the yaml configuration file
 def load_yaml(filepath):
     with open(filepath, "r") as stream:
         try:
@@ -13,17 +14,30 @@ def load_yaml(filepath):
 
 sender = "NASA <NASA@totallyNASA.com>"
 receiver = "A Test User <to@example.com>"
-
 endpoint = "https://api.nasa.gov/planetary/apod"
+
+# load the configuration file contents
 config = load_yaml("./config.yaml")
 api_key = config["NASA_api_key"]
+
+# define query parameters for the api call
 query_params = {"api_key": api_key, "date": datetime.today().strftime("%Y-%m-%d")}
+
+# make the api call
 response = requests.get(endpoint, params=query_params)
+
+# if the call is successful, the status code will be 200 and the code is going
+# to run and write the email message
 if response.status_code == 200:
+    # extract the relevant information from the api call
     explanation = response.json()["explanation"]
     title = response.json()["title"]
     media_type = response.json()["media_type"]
     url = response.json()["url"]
+
+    # according to the type of the media of the day, a message will be
+    # scripted taking into account it being a video of the day or a picture
+    # of the day
     if media_type == "image":
         message = f"""\
         Subject: Your Daily Astronomy Picture: {title}
@@ -53,5 +67,6 @@ if response.status_code == 200:
         {url}
         """
     print(message)
+# If the call is unsuccessful, the program prints to console the error
 else:
     print(f"{response.status_code}: {response.reason}")
--- NASA_Picture_Sender.py
+++ NASA_Picture_Sender.py
@@ -4,6 +4,7 @@ import requests
 import yaml
 
 
+# define a program that safely loads the data from the yaml configuration file
 def load_yaml(filepath):
     with open(filepath, "r") as stream:
         try:
@@ -14,17 +15,29 @@ def load_yaml(filepath):
 
 sender = "NASA <from@example.com>"
 receiver = "A Test User <to@example.com>"
-
 endpoint = "https://api.nasa.gov/planetary/apod"
+
+# load the configuration file contents
 config = load_yaml("./config.yaml")
+
+# define query parameters for the api call
 api_key = config["NASA_api_key"]
 query_params = {"api_key": api_key, "date": datetime.today().strftime("%Y-%m-%d")}
+
+# make the api call
 response = requests.get(endpoint, params=query_params)
+
+# if the call is successful, the status code will be 200 and the code is going
+# to run and send the email successfully
 if response.status_code == 200:
+    # extract the information from the api call
     explanation = response.json()["explanation"]
     title = response.json()["title"]
     media_type = response.json()["media_type"]
     url = response.json()["url"]
+
+    # the program checks whether the media of the day is an image or a video and
+    # adjusts the message accordingly
     if media_type == "image":
         message = f"""\
         Subject: Your Daily Astronomy Picture: {title}
@@ -53,8 +66,14 @@ if response.status_code == 200:
 
         {url}
         """
+    # Since the email will potentially contain ascii incompatible characters
+    # that would conflict with the send mail script, I get rid of them
     message = message.encode("ascii", "ignore").decode("ascii")
+
+    # Send the email using the local host server
     with smtplib.SMTP("localhost", 1025) as server:
         server.sendmail(sender, receiver, message)
+
+# If the call is unsuccessful, the program prints to console the error
 else:
     print(f"{response.status_code}: {response.reason}")
